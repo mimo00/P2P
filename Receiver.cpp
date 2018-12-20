6dd7f7791a53aa01578b666242237694d3e63d76
mimo00 <michal.mokrogulski@gmail.com>
2018-12-20 13:29:35 +0100

    Refactor.

--- Receiver.cpp
+++ Receiver.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Receiver.h"
+
+using namespace std;
+
+Receiver::Receiver(vector<Task>* tasks): tasks(tasks){};
+
+void Receiver::run()
+{
+    cout << "Task Start" << endl;
+    while (!stopRequested())
+    {
+        cout << "Doing Some Work " << tasks->size() << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+    cout << "Task End" << endl;
+}
