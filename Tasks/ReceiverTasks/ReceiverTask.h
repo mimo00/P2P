17d644a845d1e540f1b1c5cd05a655077a99d2ce
mimo00 <michal.mokrogulski@gmail.com>
2019-01-14 11:15:56 +0100

    Refactor.

---
+++ Tasks/ReceiverTasks/ReceiverTask.h
@@ -0,0 +1,23 @@
+//
+// Created by michal on 27.12.18.
+//
+
+#ifndef P2P_RECEIVERTASK_H
+#define P2P_RECEIVERTASK_H
+
+
+#include "../../Serializers/Deserializers/Input.h"
+
+class ReceiverTask {
+public:
+    ReceiverTask(int taskId);
+    virtual void handle(int operationCode, Input* input) = 0;
+    virtual void close() = 0;
+    int getTaskId() const;
+
+protected:
+    int taskId;
+    Input* input;
+};
+
+#endif //P2P_RECEIVERTASK_H
